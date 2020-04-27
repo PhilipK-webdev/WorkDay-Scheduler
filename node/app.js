b4887c208e8cbef42e5236bdc1dd153459892246
Philip Kouchner <philipk179@gmail.com>
2020-04-26 17:44:35 -0700

    array of hours

--- node/app.js
+++ node/app.js
@@ -23,36 +23,66 @@ $(document).ready(function () {
     // console.log(typeof timeInt);
 
     // var time = moment().startOf('day');
-    var time = moment().startOf('minute')
-    console.log(time);
-    var endOfMonth = moment().endOf('Month').format("MM");
-    console.log(endOfMonth);
+    // var time = moment().startOf('minute')
+    // console.log(time);
+    // var endOfMonth = moment().endOf('Month').format("MM");
+    // console.log(endOfMonth);
 
     var arrHours = [];
 
-    for (var i = 0; i < 9; i++) {
 
-        arrHours[i] = moment().start
+    for (var i = 24; i > 0; i--) {
+
+        arrHours.push(moment().add(i, "hours").format("h A"));
+
     }
 
+    var temp = moment().format("h A");
+    console.log(temp);
+    console.log(arrHours[0])
 
-    // for (var i = 0; i < 9; i++) {
+    for (var i = 0; i < 9; i++) {
 
-    //     $(".container").append(`
+        if (temp === arrHours[i]) {
 
-    //     <div class="row text-center">
-    //     <div class="col">
-    //       <div class="div hour">
-    //         <span class="show-hour"></span>
-    //       </div>
-    //     </div>
-    //     <div class="col-10">
-    //       <textarea name="" id="" cols="30" rows="10"></textarea>
-    //     </div>
-    //     <div class="col text-center">
-    //       <button class="saveBtn"></button>
-    //     </div>
-    //   </div>`);
+            $(".container").append(`
+
+                <div class="row text-center">
+                <div class="col">
+                  <div class="div hour">
+                    <span class="show-hour">${arrHours[i]}</span>
+                  </div>
+                </div>
+                <div class="col-10">
+                  <textarea class="present" name="" id="" cols="30" rows="10"></textarea>
+                </div>
+                <div class="col text-center">
+                  <button class="saveBtn"></button>
+                </div>
+              </div>`);
+        } else if (temp !== arrHours[i]) {
+
+            $(".container").append(`
+
+                <div class="row text-center">
+                <div class="col">
+                  <div class="div hour">
+                    <span class="show-hour">${arrHours[i]}</span>
+                  </div>
+                </div>
+                <div class="col-10">
+                  <textarea class="past" name="" id="" cols="30" rows="10"></textarea>
+                </div>
+                <div class="col text-center">
+                  <button class="saveBtn"></button>
+                </div>
+              </div>`);
+
+        }
+
+
+    }
+
 
     // }
 
