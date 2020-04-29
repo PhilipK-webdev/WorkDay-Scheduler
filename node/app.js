d5b22a0490e1c57bd6f8a0637a2df2dea93e8d5b
Philip Kouchner <philipk179@gmail.com>
2020-04-28 22:09:23 -0700

    comments

--- node/app.js
+++ node/app.js
@@ -1,25 +1,24 @@
-// const moment = require("moment");
-
-// 
-
 $(document).ready(function () {
 
-    var currentTime = moment();    // 
-    var startTime = moment('09:00 am', "hh:mm a");
-    var endTime = moment('5:00 pm', "hh:mm a")
-    var arrHoursMoment = [];
+    var currentTime = moment();    // currentTime 
+    var startTime = moment('09:00 am', "hh:mm a"); // beginning of the day
+    var endTime = moment('5:00 pm', "hh:mm a"); // end of the day
+    var arrHoursMoment = []; // Array of hours
     var arrHtml = [];
-    arrHoursMoment.push(startTime.format("HH"));
 
+    // set the time on p element using moment()
     $("#currentDay").text(moment().format("LL HH:mm:ss"));
+
+    // using format of 24 hours to display on the page
+    arrHoursMoment.push(startTime.format("HH"));
     for (var i = 1; i < 9; i++) {
 
 
         arrHoursMoment.push(startTime.add(i, "hours").subtract(i - 1, "hours").format("HH"));
     }
 
-    startTime = moment('09:00 am', "hh:mm a");
-
+    startTime = moment('09:00 am', "hh:mm a"); //update the start time - after the for loop
+    // parse all the variables that using moment()
     var parseCurrentTime = parseInt(currentTime.hours());
     var parseStartTime = parseInt(startTime.hours());
     var parseEndTime = parseInt(endTime.hours());
@@ -29,34 +28,27 @@ $(document).ready(function () {
 
         parseArrHoursMoment[i] = parseInt(arrHoursMoment[i]);
     }
-
-    console.log(typeof parseArrHoursMoment);
-
+    // create dynamic html file
     for (var i = 0; i < arrHoursMoment.length; i++) {
-
-
-
         arrHtml.push(`
-
-       <div class="row text-center">
-       <div class="col">
-        <div class="div hour">
-          <span class="show-hour"><i class="fas fa-business-time">${arrHoursMoment[i] + ":00"}</i></span>
-     </div>
-     </div>
-       <div class="col-10">
-        <textarea class="textDisplay" data-id="${i}"  cols="30" rows="10"></textarea>
-     </div>
-     <div class="col text-center">
-    <button class="saveBtn" data-id=${i}><i class="far fa-save"> Save</i></button>
-      </div>
-    </div>`);
-
+        <div class="row text-center">
+        <div class="col">
+            <div class="div hour">
+            <span class="show-hour"><i class="fas fa-business-time">${arrHoursMoment[i] + ":00"}</i></span>
+        </div>
+        </div>
+        <div class="col-10">
+            <textarea class="textDisplay" data-id="${i}"  cols="30" rows="10"></textarea>
+        </div>
+        <div class="col text-center">
+        <button class="saveBtn" data-id=${i}><i class="far fa-save"> Save</i></button>
+        </div>
+        </div>`);
         $(".container").append(arrHtml[i]);
-
     }
 
     var classGet;
+    // conditions to set the background color of the textarea
     for (var i = 0; i < parseArrHoursMoment.length; i++) {
 
         if (parseCurrentTime > parseArrHoursMoment[i]) {
@@ -85,6 +77,7 @@ $(document).ready(function () {
         arrText.push(parseInt(getIdText.eq(i).attr("data-id")));
     }
 
+    // setting the local storage
     $(document).on("click", ".saveBtn", function () {
 
         var getIdBtn = parseInt($(this).attr("data-id"));
@@ -100,7 +93,7 @@ $(document).ready(function () {
         }
 
     });
-
+    // get from local storage
     for (var i = 0; i < arrText.length; i++) {
 
         var hours = arrHoursMoment[i];
@@ -109,10 +102,12 @@ $(document).ready(function () {
 
     }
 
+    // making new btn to delete the data from local storage and getting ready the page for next day.
     $(".container").prepend(`<div class="col-12 text-right">
     <i class="fas fa-eraser"><input class="btn btn-primary" type="reset" value="Reset"></input></i>
       </div>`)
 
+    // delete btn
     $(".btn").on("click", function () {
 
         window.localStorage.clear();
